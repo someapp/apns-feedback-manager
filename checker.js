c02318c64a10cd4aa17dbf01f9a97e9bc2c48d90
Jason LeBrun <jblebrun@gmail.com>
2013-04-30 13:04:10 -0700

    Store feedback results in a redis db

--- checker.js
+++ checker.js
@@ -1,15 +1,25 @@
-var APNSFeedbackClinet = require("apns-feedback").Client;
+var APNSFeedbackClient = require("apns-feedback").Client;
 var path = require('path');
 var async = require("async");
 var fs = require("fs");
-
+var redis = require('redis');
+var async = require("async");
+var client = redis.createClient(6499);
 var clients = {};
-function getFeedbackClient(domain, isDevelopment, callback) {
-    var client = clients[domain+'::'+isDevelopment];
+
+function Checker(opts) {
+    this.dataFile = opts.dataFile;
+    this.domain = opts.domain;
+    this.isDevelopment = opts.isDevelopment;
+}
+
+Checker.prototype.getFeedbackClient = function(callback) {
+    var self = this;
+    var client = clients[self.domain+'::'+self.isDevelopment];
     if(!client) {
         var certs_directory = path.join(process.cwd(),'certs');
-        var keyFile = path.join(certs_directory,domain+"-key.pem");
-        var certFile = path.join(certs_directory,domain+"-cert.pem");
+        var keyFile = path.join(certs_directory,self.domain+"-key.pem");
+        var certFile = path.join(certs_directory,self.domain+"-cert.pem");
 
         async.parallel({
             certData: function(done) { fs.readFile(certFile, done); },
@@ -17,48 +27,81 @@ function getFeedbackClient(domain, isDevelopment, callback) {
         }, function(err, info) {
             info.backup = true;
             info.debug = true;
-            info.name = domain;
+            info.name = self.domain;
             if(err) return callback(err);
-            var client = new APNSFeedbackClinet(info, !isDevelopment);
+            var client = new APNSFeedbackClient(info, !self.isDevelopment);
             return callback(null, client);
         });  
     }    
-}
+};
 
 
-function check(domain, isDevelopment) {
-    getFeedbackClient(domain, isDevelopment, function(err, client) {
-        if(err) {
-            console.log("Error");
-            console.log(err);
-            return;
-        }
-        client.check(function(err, results) {
+Checker.prototype.check = function(callback) {
+    var self = this;
+    if(self.dataFile) {
+        var client = new APNSFeedbackClient({ dataFile: self.dataFile});
+        client.check(callback);
+    } else {
+        self.getFeedbackClient(function(err, client) {
             if(err) {
                 console.log("Error");
                 console.log(err);
                 return;
             }
-            console.log("Got "+results.length+" results");
-            for(var i = 0; i < results.length; i++) {
-                console.log("Result "+i+": ");
-                console.log(results[i]);
-            }
-                
+            console.log(self.domain+", "+self.isDevelopment+" Got client: "+client);
+            client.check(callback);
         });
+    }
+};
+
+/*
+var printResults = function(err, results) {
+        if(err) {
+            console.log("Error");
+            console.log(err);
+            return;
+        }
+        console.log("Got "+results.length+" results");
+        for(var i = 0; i < results.length; i++) {
+            console.log("Result "+i+": ");
+            console.log(results[i]);
+        }
+};
+*/
+
+var storeResults = function(err, results, key, callback) {
+        if(err) {
+            console.log("Error");
+            console.log(err);
+            return;
+        }
+        var now = Date.now();
+        async.forEach(results, function(result, done) {
+            client.zadd('apns_feedback:'+key, now, JSON.stringify(result), done);
+        }, callback);
+};
+
+function checkAndStore(domain, isDevelopment, callback) {
+    var checker = new Checker({ domain: domain, isDevelopment: isDevelopment});
+    checker.check(function(err, results) {
+        if(err) {
+            console.log(err);
+            return callback(err);
+        }
+        storeResults(null, results, domain+(isDevelopment?"-sandbox":""), callback);
     });
 }
 
+var modes = ['production', 'development'];
+var app_ids = ['io.incredible.DoHome', 'io.incredible.DoHomeInternal', 'io.incredible.donna'];
 
-check('production-io.incredible.DoHome', false);
-check('development-io.incredible.DoHome', false);
-check('production-io.incredible.DoHomeInternal', false);
-check('development-io.incredible.DoHomeInternal', false);
-check('production-io.incredible.donna', false);
-check('development-io.incredible.donna', false);
-check('production-io.incredible.DoHome', true);
-check('development-io.incredible.DoHome', true);
-check('production-io.incredible.DoHomeInternal', true);
-check('development-io.incredible.DoHomeInternal', true);
-check('production-io.incredible.donna', true);
-check('development-io.incredible.donna', false);
+async.forEach(modes, function(mode, done) {
+    async.forEach(app_ids, function(app_id, done) {
+        async.forEach([true, false], function(isDevelopment, done) {
+            checkAndStore(mode+"-"+app_id, isDevelopment, done);
+        }, done); 
+    }, done);
+}, function(err) {
+    if(err) console.log(err);
+    process.exit(-1);
+});
